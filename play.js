6f7b3100d9cd59f7c08222fcab37b400a73fff49
Deepak Mane <deepak.mane123@gmail.com>
2019-03-15 23:14:28 -0700

    add new file play.js

---
+++ play.js
@@ -0,0 +1,18 @@
+var name = 'Max';
+var age = 29;
+var hashobbies = true;
+
+
+function summarizeUser(userName, userAge, userHasHobby) {
+    return (
+        'Name is ' +
+        userName +
+    ',age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+    );
+}
+
+
+console.log((name, age, hashobbies));
