ccccbc33e64e1654ca1f964b7a571be072456d66
deepak-mane <deepak.mane123@gmail.com>
2019-03-19 23:12:05 -0700

    added server file

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+// function rqListener(req, res) {
+// }
+// http.createServer(rqListener);
+
+// http.createServer(function(req, res) {
+
+// });
+
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('</html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!!!</body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
